056cf2959dbf4245cb136799e8005257a2503c79
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2017-02-11 14:35:03 +0100

    Added multi writers example with UDP!

---
+++ include/utest/test_writer/udp.hpp
@@ -0,0 +1,116 @@
+/*!
+ * @copright
+ * Copyright (c) 2017, Tymoteusz Blazejczyk
+ * All rights reserved.
+ *
+ * @copright
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * @copright
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * @copright
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * @copright
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * @copright
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file utest/test_writer/generic.hpp
+ *
+ * @brief Test writer interface
+ */
+
+#ifndef UTEST_TEST_WRITER_UDP_HPP
+#define UTEST_TEST_WRITER_UDP_HPP
+
+#include <utest/test_writer.hpp>
+
+namespace utest {
+namespace test_writer {
+
+class UDP final : public TestWriter {
+public:
+    static constexpr TestString DEFAULT_ADDRESS{"127.0.0.1"};
+    static constexpr TestSize   DEFAULT_PORT{8080};
+
+    UDP(UDP&& other) noexcept;
+
+    UDP& operator=(UDP&& other) noexcept;
+
+    UDP(TestSize port) noexcept;
+
+    UDP(const TestString& address = DEFAULT_ADDRESS,
+            TestSize port = DEFAULT_PORT) noexcept;
+
+    virtual void write(const TestString& str) noexcept override;
+
+    virtual void color(TestColor c) noexcept override;
+
+    virtual ~UDP() noexcept override;
+private:
+    UDP(const UDP&) = delete;
+    UDP& operator=(const UDP&) = delete;
+
+    template<typename T = void>
+    void context(T* ptr) noexcept;
+
+    template<typename T = void>
+    T* context() noexcept;
+
+    void* m_context{nullptr};
+};
+
+inline
+UDP::UDP(UDP&& other) noexcept :
+    m_context{other.context()}
+{
+    other.context<void>(nullptr);
+}
+
+inline auto
+UDP::operator=(UDP&& other) noexcept -> UDP& {
+    if (this != &other) {
+        context(other.context());
+        other.context<void>(nullptr);
+    }
+    return *this;
+}
+
+inline
+UDP::UDP(TestSize port) noexcept :
+    UDP{DEFAULT_ADDRESS, port}
+{ }
+
+template<typename T> inline void
+UDP::context(T* ptr) noexcept {
+    m_context = static_cast<void*>(ptr);
+}
+
+template<typename T> inline auto
+UDP::context() noexcept -> T* {
+    return static_cast<T*>(m_context);
+}
+
+}
+}
+
+#endif /* UTEST_TEST_WRITER_GENERIC_HPP */
---
+++ src/utest/test_writer/linux/udp.cpp
@@ -0,0 +1,99 @@
+/*!
+ * @copright
+ * Copyright (c) 2017, Tymoteusz Blazejczyk
+ * All rights reserved.
+ *
+ * @copright
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * @copright
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * @copright
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * @copright
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * @copright
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file utest/test_writer/udp.cpp
+ *
+ * @brief Test writer UDP implementation
+ */
+
+#include <utest/test_writer/udp.hpp>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <new>
+#include <cstdint>
+
+using utest::test_writer::UDP;
+
+struct Socket {
+    int fd;
+    sockaddr_in other;
+};
+
+UDP::UDP(const TestString& address, TestSize port) noexcept {
+    sockaddr_in other = {};
+    auto fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (fd != -1) {
+        other.sin_family = AF_INET;
+        other.sin_port = htons(std::uint16_t(port));
+
+        if (!inet_aton(address.data(), &other.sin_addr)) {
+            close(fd);
+            fd = -1;
+        }
+    }
+
+    if (fd != -1) {
+        context<Socket>(new (std::nothrow) Socket);
+        if (context<Socket>()) {
+            *context<Socket>() = {fd, other};
+        }
+    }
+}
+
+void UDP::write(const TestString& str) noexcept {
+    if (context<Socket>()) {
+        auto status = sendto(context<Socket>()->fd, str.data(), str.size(), 0,
+                reinterpret_cast<sockaddr*>(&context<Socket>()->other),
+                sizeof(sockaddr_in));
+        if (status == -1) {
+            this->~UDP();
+        }
+    }
+}
+
+void UDP::color(TestColor) noexcept { }
+
+UDP::~UDP() noexcept {
+    if (context()) {
+        close(context<Socket>()->fd);
+        delete context<Socket>();
+        context<void>(nullptr);
+    }
+}
