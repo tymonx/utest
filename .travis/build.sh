2785421fa42184adeb41f6b020d9f2ed1f1cacff
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2017-02-12 21:23:46 +0100

    Find proper gcov version using compiler prefix and suffix

--- .travis/build.sh
+++ .travis/build.sh
@@ -91,7 +91,7 @@ clang-arm-none-eabi)
     mkdir -p build && cd build
     cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-clang-arm-none-eabi.cmake \
         -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-        -DEXAMPLES=ON -DSEMIHOSTING=ON -DTESTS=ON \
+        -DEXAMPLES=ON -DSEMIHOSTING=ON -DTESTS=ON -DLTO=OFF \
         -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && make
     make test
 
--- cmake/AddCodeCoverage.cmake
+++ cmake/AddCodeCoverage.cmake
@@ -32,32 +32,55 @@ if (CMAKE_BUILD_TYPE MATCHES "Coverage")
         message(FATAL_ERROR "Code coverage is only supported by GNU compiler")
     endif()
 
+    get_filename_component(COMPILER_ROOT ${CMAKE_C_COMPILER} DIRECTORY)
+    get_filename_component(COMPILER_NAME ${CMAKE_C_COMPILER} NAME)
+
+    string(REGEX REPLACE "([a-z0-9-]*)gcc.*" "\\1" COMPILER_PREFIX ${COMPILER_NAME})
+    string(REGEX REPLACE ".*gcc([a-z0-9-]*)" "\\1" COMPILER_SUFFIX ${COMPILER_NAME})
+
+    find_program(GCOV_COMMAND "${COMPILER_PREFIX}gcov${COMPILER_SUFFIX}"
+        HINTS ${COMPILER_ROOT} PATHS ${COMPILER_ROOT})
+
+    if (NOT GCOV_COMMAND)
+        message(FATAL_ERROR "Cannot find gcov command")
+    endif()
+
+    message(STATUS "Found gcov: ${GCOV_COMMAND}")
+
     find_program(LCOV_COMMAND lcov)
     if (NOT LCOV_COMMAND)
         message(FATAL_ERROR "Cannot find lcov command")
     endif()
+
+    message(STATUS "Found lcov: ${LCOV_COMMAND}")
 
     find_program(GENHTML_COMMAND genhtml)
     if (NOT GENHTML_COMMAND)
         message(FATAL_ERROR "Cannot find genhtml command")
     endif()
 
+    message(STATUS "Found genhtml: ${GENHTML_COMMAND}")
+
     add_custom_target(code_coverage
         COMMAND ${LCOV_COMMAND}
             --capture
+            --gcov-tool ${GCOV_COMMAND}
             --initial
             --directory .
             --output-file coverage.base
         COMMAND ${LCOV_COMMAND}
             --capture
+            --gcov-tool ${GCOV_COMMAND}
             --directory .
             --output-file coverage.run
         COMMAND ${LCOV_COMMAND}
+            --gcov-tool ${GCOV_COMMAND}
             --add-tracefile coverage.base
             --add-tracefile coverage.run
             --directory .
             --output-file coverage.total
         COMMAND ${LCOV_COMMAND}
+            --gcov-tool ${GCOV_COMMAND}
             --extract coverage.total */src/*.cpp
             --extract coverage.total */src/*.hpp
             --extract coverage.total */include/utest/*.hpp
